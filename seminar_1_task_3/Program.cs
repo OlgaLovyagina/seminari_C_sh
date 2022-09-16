650dd785e43347bd9b7dd15225f7045eb250d9a5
Olga <ol_0524@mail.ru>
2022-09-16 23:53:26 +0300

    пример задачи на цикл

---
+++ seminar_1_task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пример задачи на цикл
+// Пока время раннее, ещё можно смотреть мультики
+int time = 8;
+while(time < 22)
+{
+    time++; // ++ заменяет time = time + 1
+    Console.WriteLine("ещё можно смотреть мультики. Время: " + time);
+}
+Console.WriteLine("пора спать");
+Console. WriteLine("время:" + time);
+
+
