e46c4885c5935ea440b04a34bbb97723a05b5e49
Olga <ol_0524@mail.ru>
2022-09-17 00:24:21 +0300

    задача 4, пример задачи на ветвление

---
+++ seminar_1_task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет,
+//является ли первое число квадратом второго.
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = b * b;
+if (a == c)
+{   
+    Console. WriteLine("Да, первое число является квадратом второго");
+}
+else
+{
+    Console. WriteLine("Нет, первое число не является квадратом второго");
+}
