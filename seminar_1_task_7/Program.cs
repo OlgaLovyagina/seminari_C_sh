36987b0541541080fc575ffbeb9fae7101b591f1
Olga <ol_0524@mail.ru>
2022-09-17 22:41:47 +0300

    задача 7, вывод целых чисел в промежутке, модуль

---
+++ seminar_1_task_7/Program.cs
@@ -0,0 +1,13 @@
+﻿// 7. Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console. ReadLine());
+num = Math.Abs(num); // используем модуль, так как введенное число может быть отрицательное
+int nam1 = -num;
+while(nam1 <= num)
+{
+    Console.Write(nam1 + "  ");
+    nam1 = nam1 + 1;
+}
+Console.WriteLine();
+
