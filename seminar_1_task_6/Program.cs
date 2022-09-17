d9746d69f52e35fda54a5ae0fa8aa241f96ceee6
Olga <ol_0524@mail.ru>
2022-09-17 21:19:38 +0300

    задача 6, функция вычисления квадрата числа

---
+++ seminar_1_task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿// 5. Напишите программу вычисления значения функции возведения числа в квадрат.
+int Power(int x)
+{
+    return x*x;
+}
+Console.WriteLine("Введите число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Power(namber));
+
+
